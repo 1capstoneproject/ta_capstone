b856f794e481709aac97e921e5bc6e6b898b7875
tugas akhir <atikhidayati178@gmail.com>
2024-06-18 10:58:12 +0700

    add page payment

--- lib/presentation/pages/payment/payment.dart
+++ lib/presentation/pages/payment/payment.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ta_capstone/presentation/pages/check_out/checkout_paket.dart';
+import 'package:ta_capstone/presentation/widget/cart_payment.dart';
 import 'package:ta_capstone/share/app_style/style.dart';
 
 import '../../../share/app_colors/colors.dart';
 import '../../controller/detail_paket_controller.dart';
+import '../../widget/button.dart';
 
 class Pembayaran extends StatelessWidget {
   const Pembayaran({super.key});
@@ -86,12 +88,56 @@ class Pembayaran extends StatelessWidget {
                     title: '',
                     decs: 'Terapkan',
                   ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Metode Pembayaran',
+                    style: titleMedium,
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/qris.png',
+                    name: 'QRIS',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/Ovo.png',
+                    name: 'OVO',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/gopay.png',
+                    name: 'GOPAY',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/shopee.png',
+                    name: 'SHOPEE',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/linkaja.png',
+                    name: 'LINKAJA',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/dana.png',
+                    name: 'DANA',
+                  ),
+                  const CartPayment(
+                    img: 'assets/images/cod.png',
+                    name: 'COD',
+                  ),
                 ],
               ),
             ),
           ],
         ),
       ),
+      persistentFooterButtons: [
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: ButtonComponent(
+            title: 'Beli Sekarang',
+            onPressed: () {},
+          ),
+        ),
+      ],
     );
   }
 }
@@ -177,6 +223,7 @@ class ButtonTotal extends StatelessWidget {
                       ),
                       border: OutlineInputBorder(),
                       counterText: '',
+                      hintText: 'Masukkan Kode Kupon',
                       hintStyle: bodySmallGrey,
                     ),
                   ),
--- lib/presentation/widget/cart_payment.dart
+++ lib/presentation/widget/cart_payment.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ta_capstone/share/app_style/style.dart';
 
 class CartPayment extends StatelessWidget {
-  const CartPayment({super.key});
+  const CartPayment({
+    super.key,
+    required this.img,
+    required this.name,
+  });
+
+  final String img;
+  final String name;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        right: 20,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: Get.width * 0.2,
+            height: Get.width * 0.1,
+            // color: Colors.red,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      img,
+                    ),
+                    fit: BoxFit.contain),
+                borderRadius: BorderRadius.circular(5)),
+          ),
+          Spacer(),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            name,
+            style: labelMedium,
+          ),
+        ],
+      ),
+    );
   }
 }
